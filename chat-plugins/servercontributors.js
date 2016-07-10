3017451061f75e20a1530e437793bb832ea3a591
Darkeavile <loko_katto@hotmail.com>
2016-07-10 01:59:46 -0400

    Update servercontributors.js

--- chat-plugins/servercontributors.js
+++ chat-plugins/servercontributors.js
@@ -10,15 +10,15 @@ hashColor = function(name, bold) {
 exports.commands = {
 	credit: 'credits',
 	credits: function (target, room, user) {
-		this.popupReply("|html|" + "<font size=5><center>Creditos Del Servidor</center></font><br />" +
+		this.popupReply("|html|" + "<font size=5><center>Créditos Del Servidor</center></font><br />" +
 					"<u>Dueños:</u><br />" +
-					"- " + hashColor('Fantasmano', true) + " (Fundador, CSS del server)<br />" +
+					"- " + hashColor('Darkeavile', true) + " (Fundador, Diseñador de Avatars)<br />" +
 					"<br />" +
 					"<u>Desarrollo:</u><br />" +
-					"- " + hashColor('Kevinxzllz', true) + " (Contribuidor)<br />" +
+					"- " + hashColor('Fantasmano', true) + " (Contribuyente técnico)<br />" +
 					"<br />" +
 					"<u>Agradecimientos especiales:</u><br />" +
-					"- Equipo de personal actual<br />" +
+					"- Staff actual<br />" +
 					"- Nuestros usuarios<br />");
 	},
 };
